d0c8ccde171bb32ce611b41850aa1105e55f11ea
Sofiene Laadhari <laadhari.sofiene@gmail.com>
2017-09-19 01:20:42 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  scope module: :api do
+    scope module: :v1 do
+      resources :apps, path: '/api/1/apps', only: [:edit, :destroy]
+      resources :apps, path: '/api/1/apps', only: [:create, :update], defaults: {format: 'json'}
+    end
+  end
 end
