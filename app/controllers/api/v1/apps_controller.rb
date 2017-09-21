864d51f762f0202eb6214943a6be3ca5bedd7217
Sofiene Laadhari <laadhari.sofiene@gmail.com>
2017-09-21 23:56:21 +0200

    update apps controller

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -2,25 +2,35 @@ class API::V1::AppsController < ApplicationController
   protect_from_forgery prepend: true
 
   def create
-    respond_to do |format|
-      format.json { render json: {} }
-    end
-  end
-
-  def edit
-    respond_to do |format|
-      format.json { render json: {} }
-    end
+    App.create!(data: sanitized_params)
+    render body: nil, status: 204
   end
 
   def update
-    respond_to do |format|
-      format.json { render json: {} }
-    end
-  end
-
-  def destroy
-    App.find(params[:id]).destroy
+    App.find(params[:id]).update_attributes!(data: sanitized_params)
     render body: nil, status: 204
   end
+
+
+  def destroy
+    App.find(params[:id]).destroy!
+    render body: nil, status: 204
+  end
+
+  private
+
+  def sanitized_params
+    {
+      name: post_params[:name],
+      image: post_params[:image],
+      link: post_params[:link],
+      category: post_params[:category],
+      rank: post_params[:rank].match(/^\d+$/) ? post_params[:rank].to_i : "",
+    }
+  end
+
+  def post_params
+    @post_params ||= params.permit(:id, :name, :image, :link, :category, :rank)
+  end
+
 end
